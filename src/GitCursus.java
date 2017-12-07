659b1662a140b7e456846d1811dff768c76b795b
Maurice Geurden <emgy@telenet.be>
2017-12-07 14:19:12 +0100

    bonjour

--- src/GitCursus.java
+++ src/GitCursus.java
@@ -15,8 +15,8 @@ public class GitCursus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hallo");
-        System.out.println("Even het een en ander testen");
+        System.out.println("Bonjour");
+        
     }
     
 }
